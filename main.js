29b9e1cc211f0aecd72a644824d8ea2680e4e6c7
kumasan903 <kawanishi.souta.20050718@gmail.com>
2023-02-16 23:26:08 +0900

    rename variable

--- main.js
+++ main.js
@@ -176,20 +176,20 @@ function create_new_sep(){
 }
 
 function change_color(obj) {
-	let parent_strip_classname = obj.parentNode.parentNode.className;
-	if (parent_strip_classname === "dep_strip") {
+	let parent_strip_className = obj.parentNode.parentNode.className;
+	if (parent_strip_className === "dep_strip") {
 		obj.parentNode.parentNode.className = "arr_strip";
 	}
-	if (parent_strip_classname == "arr_strip") {
+	if (parent_strip_className == "arr_strip") {
 		obj.parentNode.parentNode.className = "other_strip";
 	}
-	if (parent_strip_classname == "other_strip") {
+	if (parent_strip_className == "other_strip") {
 		obj.parentNode.parentNode.className = "dep_strip";
 	}
-	if (parent_strip_classname == "sep") {
+	if (parent_strip_className == "sep") {
 		obj.parentNode.parentNode.className = "red_sep"
 	}
-	if (parent_strip_classname == "red_sep") {
+	if (parent_strip_className == "red_sep") {
 		obj.parentNode.parentNode.className = "sep"
 	}
 }
