67bf882ab35d933dc13d29322b72fd38fc989ffd
kumasan903 <kawanishi.souta.20050718@gmail.com>
2023-03-22 03:23:12 +0900

    時計を追加。ウィンドウを離れるときに警告を表示

--- main.js
+++ main.js
@@ -193,3 +193,20 @@ function change_color(obj) {
 		obj.parentNode.parentNode.className = "sep"
 	}
 }
+
+function clock()
+{
+	const date = new Date();
+	const hours = ("0" + date.getUTCHours()).slice(-2);
+	const minutes = ("0" + date.getUTCMinutes()).slice(-2);
+	const time = hours + minutes + 'z';
+	const clock = document.getElementById("clock");
+	clock.value = time;
+}
+
+setInterval(clock, 1000);
+
+window.onbeforeunload = function(event)
+{
+	return("ページを閉じてもデータは保存されません。");
+}
