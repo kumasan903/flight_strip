fe2ffdf4fc8e8f16995d495051babb4bf035f9b6
kumasan903 <kawanishi.souta.20050718@gmail.com>
2023-04-26 19:09:48 +0900

    スコークコードを自動で割り当て

--- main.js
+++ main.js
@@ -204,6 +204,35 @@ function clock()
 	clock.value = time;
 }
 
+// on change from_airport
+document.addEventListener('change', function(event) {
+	if (event.target.classList.contains('div7')) {
+		set_squawk(event.target);
+	}
+});
+
+let rjtt_squawk = 705; //1301
+
+function set_squawk(object)
+{
+	let squawk = object.previousSibling.previousSibling.previousSibling;
+	// もし、スコーク欄が空欄なら
+	if (squawk.value === "")
+	{
+		// 出発空港によってスコークを割り当てる
+		if (object.value === "RJTT")
+		{
+			squawk.value = ( '0000' + rjtt_squawk.toString(8)).slice(-4);
+			rjtt_squawk += 1;
+		}
+		else
+		{
+			squawk.value = ( '0000' + rjtt_squawk.toString(8)).slice(-4);
+			rjtt_squawk += 1;
+		}
+	}
+}
+
 setInterval(clock, 1000);
 
 window.onbeforeunload = function(event)
